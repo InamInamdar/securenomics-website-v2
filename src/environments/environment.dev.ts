cffc657f204a5af34ecaeddc62a70e5c7de07e44
Abid Shaikh <99531961+abidShaikh08@users.noreply.github.com>
2026-03-13 02:04:06 +0530

    Reverted the urls back to prod

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,8 +2,11 @@ export const environment = {
 	production: false,
 
 	baseUrls: {
-		VOUCHER_APP_URL: "https://vouchers.dev-comanage360.com/#/",
-		SDLP_EXPOSED_URL: "https://snlp-api.dev-comanage360.com/",
-		REGISTRATION_URL: "https://registration-api.dev-comanage360.com/",
+		// VOUCHER_APP_URL: "https://vouchers.dev-comanage360.com/#/",
+		// SDLP_EXPOSED_URL: "https://snlp-api.dev-comanage360.com/",
+		// REGISTRATION_URL: "https://registration-api.dev-comanage360.com/",
+		VOUCHER_APP_URL: "https://vouchers.comanage360.com/#/",
+		SDLP_EXPOSED_URL: "https://snlp-backend.comanage360.com/",
+		REGISTRATION_URL: "https://registration-api.comanage360.com/",
 	},
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,12 @@ export const environment = {
 	production: false,
 
 	baseUrls: {
-		VOUCHER_APP_URL: "https://vouchers.dev-comanage360.com/#/",
-		SDLP_EXPOSED_URL: "https://snlp-api.dev-comanage360.com/",
-		// SDLP_EXPOSED_URL: 'http://localhost:3200/',
-		REGISTRATION_URL: "https://registration-api.dev-comanage360.com/",
+		// VOUCHER_APP_URL: "https://vouchers.dev-comanage360.com/#/",
+		// SDLP_EXPOSED_URL: "https://snlp-api.dev-comanage360.com/",
+		// REGISTRATION_URL: "https://registration-api.dev-comanage360.com/",
+		VOUCHER_APP_URL: "https://vouchers.comanage360.com/#/",
+		SDLP_EXPOSED_URL: "https://snlp-backend.comanage360.com/",
+		REGISTRATION_URL: "https://registration-api.comanage360.com/",
 	},
 };
 
